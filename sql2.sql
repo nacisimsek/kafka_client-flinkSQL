79a489b4b59dbe87e5ea332da11931f8e35d44b5
nacisimsek <naci86@gmail.com>
2025-02-13 22:25:55 +0100

    Update SQL-2 to perform windowing

--- sql2.sql
+++ sql2.sql
@@ -1,9 +1,10 @@
 SELECT
-  title,
-  DATE_FORMAT(datetime, 'yyyy-MM-dd') AS date_day,
-  COUNT(*) AS daily_view_count,
-  SUM(duration) AS daily_total_watch_time
-FROM `default`.`kafka_naci`.`netflix-uk-views`
-  WHERE `duration` > 0
-GROUP BY
-  title,DATE_FORMAT(datetime, 'yyyy-MM-dd');
+    window_start,
+    window_end,
+    title,
+    COUNT(*) AS daily_view_count,
+    SUM(duration) AS daily_total_watch_time
+FROM TABLE (
+    TUMBLE(TABLE `default`.`kafka_naci`.`netflix-uk-views`, DESCRIPTOR(datetime), INTERVAL '1' DAY)
+)
+GROUP BY window_start, window_end, title;
